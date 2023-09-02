4db5b6cf3fe9735e2a870773ba17f14717dc6b58
vaidehi0105 <vaidehibedekar0105@gmail.com>
2023-09-02 21:38:00 +0530

    new files added

--- setup.py
+++ setup.py
@@ -6,10 +6,10 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 __version__ = "0.0.0"
 
-REPO_NAME = "Chicken-Disease-Classification--Project"
-AUTHOR_USER_NAME = "entbappy"
+REPO_NAME = "PCOS_END_TO_END_WITH_Depolyment"
+AUTHOR_USER_NAME = "vaidehi0105"
 SRC_REPO = "cnnClassifier"
-AUTHOR_EMAIL = "entbappy73@gmail.com"
+AUTHOR_EMAIL = "vaidehibedekar0105@gmail.com"
 
 
 setuptools.setup(
